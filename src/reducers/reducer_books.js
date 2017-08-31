fc48f920ba88aa6e252d2c8482ce7f8212e35f42
Michael Busby <michaelb@clearlyinnovative.com>
2017-08-31 09:26:04 -0400

    connect book list to state

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Lord of the Rings'},
+        {title: 'Head-First Desing Patterns'},
+        {title: 'Javascript: The Good Parts'},
+        {title: `Dante's Inferno`},
+    ]
+}
