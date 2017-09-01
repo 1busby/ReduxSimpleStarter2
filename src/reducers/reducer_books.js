74eeaae4667a9919422b531e856d7f3174cc54c0
Michael Busby <michaelb@clearlyinnovative.com>
2017-09-01 12:03:12 -0400

    Add selectBook Actions

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,7 +1,10 @@
 export default function() {
+
+    
+
     return [
         {title: 'Lord of the Rings'},
-        {title: 'Head-First Desing Patterns'},
+        {title: 'Head-First Design Patterns'},
         {title: 'Javascript: The Good Parts'},
         {title: `Dante's Inferno`},
     ]
