3e99a474e954a9203019ac24b59dd9f591867240
Michael Busby <michaelb@clearlyinnovative.com>
2017-09-02 16:44:58 -0400

    Add selectBook action

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,9 +3,9 @@ export default function() {
     
 
     return [
-        {title: 'Lord of the Rings'},
-        {title: 'Head-First Design Patterns'},
-        {title: 'Javascript: The Good Parts'},
-        {title: `Dante's Inferno`},
+        {title: 'Lord of the Rings', pages: 840},
+        {title: 'Head-First Design Patterns', pages: 503},
+        {title: 'Javascript: The Good Parts', pages: 122},
+        {title: `Dante's Inferno`, pages: 352},
     ]
 }
